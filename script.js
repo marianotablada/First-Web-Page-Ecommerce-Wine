ecc27b74348aa263ec0792da03e69c8ecba927b2
Marian Tablada <mariantablada@Air-de-Marian.home>
2022-07-13 10:47:35 +0200

    this version include botton scroll to top

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ menu.addEventListener("click", () => {
 });
 
 
+//Captura y/o registro del boton
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 80px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
